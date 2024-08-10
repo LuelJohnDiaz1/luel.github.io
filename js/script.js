33ab5343173d0003edb6ce3a9792c3c70a80399e
Luel <Luel@DESKTOP-6511OG0>
2024-08-10 16:06:19 +0800

    Add navigation clicks properties.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('.navbar li a');
 
 menu.onclick = () => {
+    toggleMenu();
+};
+
+sections.forEach(section => {
+    section.addEventListener("click", function () {
+        if (navbar.classList.contains('open')) {
+            toggleMenu();
+        }
+    });
+});
+
+function toggleMenu() {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
-}
+};
