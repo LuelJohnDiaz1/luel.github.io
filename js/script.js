6d8975574656f81d63e0fd68eee14283be18a092
luel <lueljohndiaz@gmail.com>
2024-08-27 15:47:47 +0800

    Make the experiences collapsable

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('.navbar li a');
+let coll = document.getElementsByClassName("collapsible");
 
 menu.onclick = () => {
     toggleMenu();
@@ -18,3 +19,17 @@ function toggleMenu() {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+
+/* Collapsible function */
+var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
