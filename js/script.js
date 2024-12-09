f3ea0f46cac5ecec9d54fc1b5da848a29126b562
Luel <Luel@DESKTOP-6511OG0>
2024-12-09 14:37:12 +0800

    Add projects section.

--- js/script.js
+++ js/script.js
@@ -4,32 +4,33 @@ let sections = document.querySelectorAll('.navbar li a');
 let coll = document.getElementsByClassName("collapsible");
 
 menu.onclick = () => {
-    toggleMenu();
+  toggleMenu();
 };
 
 sections.forEach(section => {
-    section.addEventListener("click", function () {
-        if (navbar.classList.contains('open')) {
-            toggleMenu();
-        }
-    });
+  section.addEventListener("click", function () {
+    if (navbar.classList.contains('open')) {
+      toggleMenu();
+    }
+  });
 });
 
 function toggleMenu() {
-    menu.classList.toggle('bx-x');
-    navbar.classList.toggle('open');
+  menu.classList.toggle('bx-x');
+  navbar.classList.toggle('open');
 };
 
 /* Collapsible function */
 var i;
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.maxHeight){
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    } 
+    }
   });
+
 }
